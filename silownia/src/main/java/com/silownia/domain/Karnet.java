bee03684be86569bedd2cc04ccb4c17494aba82d
damian11 <damian.kasztelan1@op.pl>
2016-01-19 21:27:20 +0100

    dodanie metod do klasy Karnet.java

--- silownia/src/main/java/com/silownia/domain/Karnet.java
+++ silownia/src/main/java/com/silownia/domain/Karnet.java
@@ -15,3 +15,30 @@ public class Karnet {
 	private String opis;
 	private String cena;
 }
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+public Long getId_karnet() {
+  return id_karnet;
+}
+public void setId_karnet(Long id) {
+  this.id_karnet = id_karnet;
+}
+public String getRodzaj() {
+  return rodzaj;
+}
+public void setRodzaj(String rodzaj) {
+  this.rodzaj = rodzaj;
+}
+public String getOpis() {
+  return opis;
+}
+public void setOpis(String opis) {
+  this.opis = opis;
+}
+public void getCena() {
+  return cena;
+}
+public void setCena(String cena) {
+  this.cena = cena;
+}
+}
