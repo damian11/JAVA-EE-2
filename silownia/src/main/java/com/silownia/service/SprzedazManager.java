55547bda88363b62ab763d7a73e79ee4ea195ef4
damian11 <damian.kasztelan1@op.pl>
2016-01-19 23:08:27 +0100

    dodanie metody sprzedazKarnetu do klasy SprzedazManager.java

--- silownia/src/main/java/com/silownia/service/SprzedazManager.java
+++ silownia/src/main/java/com/silownia/service/SprzedazManager.java
@@ -12,4 +12,16 @@ import com.silownia.domain.Karnet;
 @Stateless
 public class SprzedazManager {
 
+  	@PersistenceContext
+  	EntityManager em;
+
+  	public void sprzedazKarnetu (Long id_karnet, Long id_klient) {
+
+  		Klient klient = em.find(Klient.class, id_klient);
+  		Karnet karnet = em.find(Karnet.class, id_karnet);
+
+
+  	klient.getKarnety().add(karnet);
+  	}
+
 }
