f7b0f837faf999e62ecad73937fc13a6944edb18
damian11 <damian.kasztelan1@op.pl>
2016-01-26 20:50:04 +0100

    dodanie JAXRSConfiguration.java

---
+++ silownia/src/main/java/com/silownia/rest/JAXRSConfiguration.java
@@ -0,0 +1,7 @@
+package com.silownia.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest/*")
+public class JAXRSConfiguration extends Application { }
