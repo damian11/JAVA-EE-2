c699f828dac5050984f87c9ef3d022abb45d8f50
damian11 <damian.kasztelan1@op.pl>
2016-01-26 15:54:02 +0100

    obsługa operacji KlientDAO.java

--- silownia/src/main/java/com/silownia/KlientDAO.java
+++ silownia/src/main/java/com/silownia/KlientDAO.java
@@ -5,5 +5,10 @@ import java.util.List;
 
 public interface KlientDAO {
 
+  List<Klient> getAllKlient();
+  Klient addKlient(Klient klient);
+  void deleteKlient(Klient klient);
+  Klient updateKlient(Klient klient);
+  Klient getKlientByID(int id);
 
 }
