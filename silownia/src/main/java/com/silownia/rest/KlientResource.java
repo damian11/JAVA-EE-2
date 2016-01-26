61c096107e146d78133a3a29d8ea6d02283b48de
damian11 <damian.kasztelan1@op.pl>
2016-01-26 19:25:21 +0100

    dodanie KlientResources.java

---
+++ silownia/src/main/java/com/silownia/rest/KlientResource.java
@@ -0,0 +1,18 @@
+package com.siłownia.rest;
+
+import com.silownia.KlientDAO;
+import com.silownia.domain.Klient;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.Date;
+import java.util.List;
+
+@Stateless
+@Path("/klient")
+public class KlientResource {
+
+}
